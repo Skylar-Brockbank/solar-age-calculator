573a8e00b13452928e7b53a9142c1fb3a33f0e6e
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-11 09:45:03 -0800

    pass test1 now

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,7 +1,7 @@
 import {Person} from "./../src/js/person.js";
 
 describe('Person', ()=> {
-  test('should create an instance of the person object',()=>{
+  test('should create an instance of the person object with values for each planet age',()=>{
     let person1 = new Person(25);
     expect(person1.earthAge).toBeDefined();
     expect(person1.mercuryAge).toBeDefined();
--- src/js/person.js
+++ src/js/person.js
@@ -1,3 +1,7 @@
-export function Person(){
-  
+export function Person(earthAge){
+  this.earthAge = earthAge;
+  this.mercuryAge = earthAge;
+  this.venusAge = earthAge;
+  this.marsAge = earthAge;
+  this.jupiterAge = earthAge;
 }
