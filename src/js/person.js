8b521f843be1fd723cd850ca755cbe3741f6b1a9
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-11 12:10:30 -0800

    refactor Person object

--- src/js/person.js
+++ src/js/person.js
@@ -1,8 +1,21 @@
 export function Person(earthAge,lifeExpectancy){
   this.earthAge = earthAge;
-  this.mercuryAge = earthAge*0.24;
-  this.venusAge = earthAge*0.62;
-  this.marsAge = earthAge*1.88;
-  this.jupiterAge = earthAge*11.86;
+  this.mercuryAge = this.convertAge(earthAge,"mercury");
+  this.venusAge = this.convertAge(earthAge,"venus");
+  this.marsAge = this.convertAge(earthAge,"mars");
+  this.jupiterAge = this.convertAge(earthAge,"jupiter");
   this.toExpectedDeath = Math.abs(earthAge-lifeExpectancy);
+  this.toExpectedDeathMercury = this.convertAge(this.toExpectedDeath,"mercury");
+  this.toExpectedDeathVenus = this.convertAge(this.toExpectedDeath,"venus");
+  this.toExpectedDeathMars = this.convertAge(this.toExpectedDeath,"mars");
+  this.toExpectedDeathJupiter = this.convertAge(this.toExpectedDeath,"jupiter");
 }
+Person.prototype.convertAge = function(age,planet){
+  let planetConversionFactor = {
+    "mercury":0.24,
+    "venus":0.62,
+    "mars":1.88,
+    "jupiter":11.86
+  };
+  return age*planetConversionFactor[planet];
+};
