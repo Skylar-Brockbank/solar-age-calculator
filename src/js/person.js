85a5dca42bb76d7f33c368d580421392c5058992
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-11 09:53:34 -0800

    pass test 2 now

--- src/js/person.js
+++ src/js/person.js
@@ -1,7 +1,7 @@
 export function Person(earthAge){
   this.earthAge = earthAge;
-  this.mercuryAge = earthAge;
-  this.venusAge = earthAge;
-  this.marsAge = earthAge;
-  this.jupiterAge = earthAge;
+  this.mercuryAge = earthAge*0.24;
+  this.venusAge = earthAge*0.62;
+  this.marsAge = earthAge*1.88;
+  this.jupiterAge = earthAge*11.86;
 }
