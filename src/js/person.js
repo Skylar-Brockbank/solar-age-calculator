8a4d202e4e03b424dd36ac0d6d7b3140624152d3
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-11 13:36:46 -0800

    package the constructor and prototype methods into one class

--- src/js/person.js
+++ src/js/person.js
@@ -1,21 +1,23 @@
-export function Person(earthAge,lifeExpectancy){
-  this.earthAge = earthAge;
-  this.mercuryAge = this.convertAge(earthAge,"mercury");
-  this.venusAge = this.convertAge(earthAge,"venus");
-  this.marsAge = this.convertAge(earthAge,"mars");
-  this.jupiterAge = this.convertAge(earthAge,"jupiter");
-  this.toExpectedDeath = Math.abs(earthAge-lifeExpectancy);
-  this.toExpectedDeathMercury = this.convertAge(this.toExpectedDeath,"mercury");
-  this.toExpectedDeathVenus = this.convertAge(this.toExpectedDeath,"venus");
-  this.toExpectedDeathMars = this.convertAge(this.toExpectedDeath,"mars");
-  this.toExpectedDeathJupiter = this.convertAge(this.toExpectedDeath,"jupiter");
+export class Person{
+  constructor(earthAge,lifeExpectancy){
+    this.earthAge = earthAge;
+    this.mercuryAge = this.convertAge(earthAge,"mercury");
+    this.venusAge = this.convertAge(earthAge,"venus");
+    this.marsAge = this.convertAge(earthAge,"mars");
+    this.jupiterAge = this.convertAge(earthAge,"jupiter");
+    this.toExpectedDeath = Math.abs(earthAge-lifeExpectancy);
+    this.toExpectedDeathMercury = this.convertAge(this.toExpectedDeath,"mercury");
+    this.toExpectedDeathVenus = this.convertAge(this.toExpectedDeath,"venus");
+    this.toExpectedDeathMars = this.convertAge(this.toExpectedDeath,"mars");
+    this.toExpectedDeathJupiter = this.convertAge(this.toExpectedDeath,"jupiter");
+  }
+  convertAge(age,planet){
+    let planetConversionFactor = {
+      "mercury":0.24,
+      "venus":0.62,
+      "mars":1.88,
+      "jupiter":11.86
+    };
+    return (Math.trunc((age/planetConversionFactor[planet])*10)/10);
+  }
 }
-Person.prototype.convertAge = function(age,planet){
-  let planetConversionFactor = {
-    "mercury":0.24,
-    "venus":0.62,
-    "mars":1.88,
-    "jupiter":11.86
-  };
-  return (Math.trunc((age/planetConversionFactor[planet])*10)/10);
-};
