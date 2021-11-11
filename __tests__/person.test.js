04f330e568a687e9d633d38cf3aad3637e429a02
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-11 09:33:03 -0800

    fail the test good

---
+++ __tests__/person.test.js
@@ -0,0 +1,12 @@
+import {Person} from "./../src/js/person.js";
+
+describe('Person', ()=> {
+  test('should create an instance of the person object',()=>{
+    let person1 = new Person(25);
+    expect(person1.earthAge).toBeDefined();
+    expect(person1.mercuryAge).toBeDefined();
+    expect(person1.venusAge).toBeDefined();
+    expect(person1.marsAge).toBeDefined();
+    expect(person1.jupiterAge).toBeDefined();
+  });
+});
